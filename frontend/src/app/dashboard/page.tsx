5808eedb2b7626c0ea743467d29ae351a85007dc
sukanya1426 <rahmanmahdiya2002@gmail.com>
2025-06-03 23:30:44 +0600

    Add frontend and backend

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Dashboard() {
+  return (
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      </main>
+  
+  );
+}
